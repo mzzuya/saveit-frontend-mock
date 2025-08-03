89fe78a4fe42ba2bede7c3195ead861f576c2cf5
커밋 <>
2025-08-03 14:19:45 +0900

    로그인 수정

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,13 @@
 // import { StrictMode } from 'react'
+import { GoogleOAuthProvider } from '@react-oauth/google';
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <>
+  <GoogleOAuthProvider clientId="600395796293-1ct9un3h4oq87j5defursu78o2fp9nmk.apps.googleusercontent.com">
     <App />
-  </>
+  </GoogleOAuthProvider>
 )
 
 
